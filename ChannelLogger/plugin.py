eef6aca857c1fc5592cef418052aaadd9b11b342
resistivecorpse <resistivecorpse@resistivecorpse-VirtualBox.(none)>
2012-09-10 04:06:58 -0400

    adds an on/off switch config seting for logging of joins/parts/quits

--- ChannelLogger/plugin.py
+++ ChannelLogger/plugin.py
@@ -206,10 +206,11 @@ class ChannelLogger(callbacks.Plugin):
                 self.doLog(irc, channel,
                            '*** %s is now known as %s\n', oldNick, newNick)
     def doJoin(self, irc, msg):
-        for channel in msg.args[0].split(','):
-            self.doLog(irc, channel,
-                       '*** %s <%s> has joined %s\n',
-                       msg.nick, msg.prefix, channel)
+        if(self.registryValue('hideJoinParts', msg.args[0])):
+            for channel in msg.args[0].split(','):
+                self.doLog(irc, channel,
+                           '*** %s <%s> has joined %s\n',
+                           msg.nick, msg.prefix, channel)
 
     def doKick(self, irc, msg):
         if len(msg.args) == 3:
@@ -226,14 +227,15 @@ class ChannelLogger(callbacks.Plugin):
                        '*** %s was kicked by %s\n', target, msg.nick)
 
     def doPart(self, irc, msg):
-        if len(msg.args) > 1:
-            reason = " (%s)" % msg.args[1]
-        else:
-            reason = ""
-        for channel in msg.args[0].split(','):
-            self.doLog(irc, channel,
-                       '*** %s <%s> has left %s%s\n',
-                       msg.nick, msg.prefix, channel, reason)
+        if(self.registryValue('hideJoinParts', msg.args[0])):
+            if len(msg.args) > 1:
+                reason = " (%s)" % msg.args[1]
+            else:
+                reason = ""
+            for channel in msg.args[0].split(','):
+                self.doLog(irc, channel,
+                           '*** %s <%s> has left %s%s\n',
+                           msg.nick, msg.prefix, channel, reason)
 
     def doMode(self, irc, msg):
         channel = msg.args[0]
@@ -251,17 +253,18 @@ class ChannelLogger(callbacks.Plugin):
                    '*** %s changes topic to "%s"\n', msg.nick, msg.args[1])
 
     def doQuit(self, irc, msg):
-        if len(msg.args) == 1:
-            reason = " (%s)" % msg.args[0]
-        else:
-            reason = ""
-        if not isinstance(irc, irclib.Irc):
-            irc = irc.getRealIrc()
-        for (channel, chan) in #self.lastStates[irc].channels.iteritems():
-            if msg.nick in chan.users:
-                self.doLog(irc, channel,
-                           '*** %s <%s> has quit IRC%s\n',
-                           msg.nick, msg.prefix, reason)
+        if(self.registryValue('hideJoinParts', msg.args[0])):
+            if len(msg.args) == 1:
+                reason = " (%s)" % msg.args[0]
+            else:
+                reason = ""
+            if not isinstance(irc, irclib.Irc):
+                irc = irc.getRealIrc()
+            for (channel, chan) in #self.lastStates[irc].channels.iteritems():
+                if msg.nick in chan.users:
+                    self.doLog(irc, channel,
+                               '*** %s <%s> has quit IRC%s\n',
+                               msg.nick, msg.prefix, reason)
 
     def outFilter(self, irc, msg):
         # Gotta catch my own messages *somehow* :)
